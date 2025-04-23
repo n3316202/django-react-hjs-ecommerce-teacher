b63d6e961801b81c4dc12babda943255d4c9e647
heo <admin@admin.com>
2025-04-23 14:11:38 +0900

    commit

---
+++ react-vite-hjs-ecommerce/src/ui/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+
+//dev_2_fruit
+const MainLayout = () => {
+    return (
+        <div className="vh-100 d-flex flex-column justify-content-between">
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default MainLayout
