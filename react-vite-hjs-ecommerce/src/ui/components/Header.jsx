b3fd846c037f87fcc1396e47310c81da6aee52df
heo <admin@admin.com>
2025-05-02 15:33:27 +0900

    commit

--- react-vite-hjs-ecommerce/src/ui/components/Header.jsx
+++ react-vite-hjs-ecommerce/src/ui/components/Header.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom"
 import heroImg1 from '@/assets/fruits/img/hero-img-1.png'
 import heroImg2 from '@/assets/fruits/img/hero-img-2.jpg'
 import { useAuth } from "@/contexts/AuthContext"
+import { Link } from 'react-router-dom';
 
 //dev_2_fruit
 const Header = () => {
@@ -9,6 +10,10 @@ const Header = () => {
   //dev_5_Fruit
   const{user,logout} = useAuth();
   console.log(user)
+
+  const logoutHandle = () =>{
+    logout()
+  }
 
 return (
         <>
@@ -33,9 +38,19 @@ return (
           </small>
           <small className="me-3">
             <i className="fas fa-envelope me-2 text-secondary" />
-            <a href="#" className="text-white">
-              Email@Example.com
-            </a>
+            {/* dev_5_Fruit */}
+            {user ? (
+                  <span
+                    className="text-white"
+                    style={{ cursor: "pointer" }}
+                    onClick={logoutHandle}
+                  >
+                    {user.username}님 환영합니다.
+                  </span>
+                ) : (
+                  <Link to="/login" className="text-white">로그인을 해주세요</Link>
+            )}
+
           </small>
         </div>
         <div className="top-link pe-2">
