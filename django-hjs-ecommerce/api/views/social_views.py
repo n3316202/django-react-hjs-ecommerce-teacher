8472d0c8e016a5840ec23c46d31d059bbcb19153
heo <admin@admin.com>
2025-05-12 15:35:42 +0900

    commit

--- django-hjs-ecommerce/api/views/social_views.py
+++ django-hjs-ecommerce/api/views/social_views.py
@@ -1,6 +1,6 @@
 from allauth.socialaccount.providers.kakao.views import KakaoOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
 
-#dev_9_1
+#dev_9_1_Fruit
 class KakaoLoginView(SocialLoginView):
     adapter_class = KakaoOAuth2Adapter
