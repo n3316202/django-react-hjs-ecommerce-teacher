9e9c959bf3b96c5a504e9adf6759c23552f32210
heo <admin@admin.com>
2025-05-15 10:13:34 +0900

    commit

--- react-vite-hjs-ecommerce/src/ui/components/Header.jsx
+++ react-vite-hjs-ecommerce/src/ui/components/Header.jsx
@@ -21,15 +21,7 @@ const Header = () => {
   const { getTotalItems} = useCart()
 
 return (
-        <>
-  {/* Spinner Start */}
-  {/* <div
-    id="spinner"
-    className="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center"
-  >
-    <div className="spinner-grow text-primary" role="status" />
-  </div> */}
-  {/* Spinner End */}
+  <>
   {/* Navbar start */}
   <div className="container-fluid fixed-top">
     <div className="container topbar bg-primary d-none d-lg-block">
