e8f80b98e8413d174a2e3282a9f43acff7b3a956
heo <admin@admin.com>
2025-05-16 15:56:15 +0900

    commit

--- react-vite-hjs-ecommerce/src/ui/components/fruits/Shop.jsx
+++ react-vite-hjs-ecommerce/src/ui/components/fruits/Shop.jsx
@@ -8,7 +8,7 @@ import { getProductMaxPrice } from "@/api/ProductApi"
 //dev_10_Fruit
 const Shop = () => {
 
-const {setSearch,products,setOdering,setCategory,totalCount,currentPage,setCurrentPage} = useShop()
+const {setSearch,products,setOdering,setCategory,totalCount,currentPage,setCurrentPage,setMaxPrice,setMinPrice} = useShop()
 //categories = null
 const [categories, setCategories] = useState([])
 
@@ -31,9 +31,17 @@ useEffect(  () => {
 const handleSearchChange = (event)=>{
   setSearch(event.target.value); 
   setCategory("")
+
+  setCurrentPage(1)
 }
-const handleOrderingChange = (event)=>{setOdering(event.target.value)}
-const handleCategoryClick =(categoryId) => {setCategory(categoryId)}          
+const handleOrderingChange = (event)=>{
+  setOdering(event.target.value)
+  setCurrentPage(1)
+}
+const handleCategoryClick =(categoryId) => {
+  setCategory(categoryId);
+  setCurrentPage(1)
+}          
                         // <Pagination
                         //   activePage={currentPage}
                         //   itemsCountPerPage={12}
@@ -101,6 +109,15 @@ const handlePriceChange = (e) => {
   const value = parseFloat(e.target.value)
 
   setPrice(value) 
+  
+  //min=0 값과 max 값 으로 필터링
+  setMinPrice(0)
+  setMaxPrice(value)
+  
+  //슬라이더 값 변경시 카테고리는 초기화
+  setCurrentPage(1)
+  setCategory("")
+
 }
 
 
